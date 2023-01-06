dd44812b7085883d4f1b0d6a1de257c62a4d9841
Herminos <114343654+Herminos@users.noreply.github.com>
2023-01-06 12:51:01 +0800

    Update Option.java

--- src/main/java/org/launch/Option.java
+++ src/main/java/org/launch/Option.java
@@ -11,7 +11,13 @@ public class Option
     {
         System.out.println("Type \"v\" to set the version(defaults to 1.11.2)");
         System.out.println("Type \"n\" to set the playername(defaults to \"ID\")");
-        System.out.println("Type \"d\" to set the Minecraft game directory(defaults to \"minecraft\"(Warning:Most Windows computer's Minecraft directory is\".minecraft\")");
+        System.out.println("Type \"d\" to set the Minecraft game directory(defaults to \"minecraft\"(Warning:Most Windows computer's Minecraft directory is\".minecraft\"))");
+        System.out.println("Type \"advhelp\" to get the advance option-setting help");
+    }
+    public static void GetAdvanceHelp()
+    {
+        System.out.println("Type \"maxm\" to set the JVM's MaxMemory");
+        System.out.println("Type \"minm\" to set the JVM's MinMemory");
     }
     public static void SetOptions()
     {
@@ -19,6 +25,8 @@ public class Option
         String version="1.11.2";
         String Directory="minecraft";
         String PlayerName="ID";
+        int MaxMemory=0;
+        int MinMemory=0;
         while(true)
         {
             Scanner scanner=new Scanner(System.in);
@@ -26,30 +34,61 @@ public class Option
             System.out.println(input);
             if(Objects.equals(input, "v"))
             {
-                System.out.println("Set version");
+                System.out.println("Set Version");
                 Scanner scanner1=new Scanner(System.in);
                 version=scanner1.nextLine();
+                System.out.println("New Version:"+version);
             }
             else if(Objects.equals(input, "n"))
             {
-                System.out.println("Set playername");
+                System.out.println("Set Playername");
                 Scanner scanner1=new Scanner(System.in);
                 PlayerName=scanner1.nextLine();
+                System.out.println("New Playername:"+PlayerName);
             }
             else if(Objects.equals(input, "d"))
             {
-                System.out.println("Set directory");
+                System.out.println("Set Directory");
                 Scanner scanner1=new Scanner(System.in);
                 Directory=scanner1.nextLine();
+                System.out.println("New Directory:"+Directory);
+            }
+            else if(Objects.equals(input,"maxm"))
+            {
+                System.out.println("Set JVM's MaxMemory(MB)(0 for default)");
+                Scanner scanner1=new Scanner(System.in);
+                MaxMemory=scanner1.nextInt();
+                if(Objects.equals(MaxMemory,0))
+                    System.out.println("New JVM's MaxMemory: default");
+                else
+                    System.out.println("New JVM's MaxMemory: "+MaxMemory);
+            }
+            else if(Objects.equals(input,"minm"))
+            {
+                System.out.println("Set JVM's MinMemory(MB)(0 for default)");
+                Scanner scanner1=new Scanner(System.in);
+                MinMemory=scanner1.nextInt();
+                if(Objects.equals(MaxMemory,0))
+                    System.out.println("New JVM's MinMemory: default");
+                else
+                    System.out.println("New JVM's MinMemory: "+MinMemory);
             }
             else if(Objects.equals(input, "help"))
             {
                 GetHelp();
             }
+            else if(Objects.equals(input,"advhelp"))
+            {
+                GetAdvanceHelp();
+            }
             else if(Objects.equals(input, "launch"))
             {
-                LaunchGame(version,PlayerName,Directory);
+                LaunchGame(version,PlayerName,Directory,MaxMemory,MinMemory);
                 break;
+            }
+            else
+            {
+                System.out.println("Unknown Command,Type \"help\" to check your syntax");
             }
         }
     }
